eb0ad61c39982c40ac15500df1d0f81137d5e2c2
Jorge Juan Barroso Carmona <jorge@karumi.com>
2017-10-21 12:47:35 +0200

    Add agenda test.

---
+++ src/test/kotlin/com/karumi/katagenda/domain/AgendaTest.kt
@@ -0,0 +1,57 @@
+package com.karumi.katagenda.domain
+
+import com.karumi.katagenda.common.repository.InMemoryDataSource
+import com.karumi.katagenda.domain.repository.ContactsRepository
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AgendaTest {
+    companion object {
+
+        private val ANY_FIRST_NAME = "Pedro Vicente"
+        private val ANY_LAST_NAME = "Gomez Sanchez"
+        private val ANY_PHONE_NUMBER = "666666666"
+    }
+
+    @Test
+    fun shouldReturnAnEmptyListOfContactsIfTheAgendaIsEmpty() {
+        val agenda = givenAnAgenda()
+
+        val contacts = agenda.contacts
+        
+        assertTrue(contacts.isEmpty())
+    }
+
+    @Test
+    fun shouldReturnTheContactCreatedOnContactAdded() {
+        val agenda = givenAnAgenda()
+        val contactToAdd = givenAnyContact()
+
+        val createdContact = agenda.addContact(contactToAdd)
+
+        assertEquals(contactToAdd, createdContact)
+    }
+
+    @Test
+    fun shouldReturnTheNewContactAfterTheCreationUsingGetContacts() {
+        val agenda = givenAnAgenda()
+        val contact = givenAnyContact()
+
+        agenda.addContact(contact)
+
+        val contacts = agenda.contacts
+        assertTrue(contacts.contains(contact))
+        assertEquals(1, contacts.size)
+    }
+
+    private fun givenAnyContact(): Contact {
+        return Contact(ANY_FIRST_NAME, ANY_LAST_NAME, ANY_PHONE_NUMBER)
+    }
+
+    private fun givenAnAgenda(): Agenda {
+        val dataSource = InMemoryDataSource<Contact>()
+        val contactsRepository = ContactsRepository(dataSource)
+        return Agenda(contactsRepository)
+    }
+}
